89e5d93254c8eed11508af3c846c6fde47075d16
addinnt <addin309@gmail.com>
2020-02-17 14:25:12 +0700

    Task Implicit Intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -21,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
     public void handleExplicitIntent(View view) {
         Intent intent = new Intent(this, ExplicitIntentActivity.class);
         startActivity(intent);
+
+    }
+
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void handleExit(View view) {
+        finish();
     }
 }
